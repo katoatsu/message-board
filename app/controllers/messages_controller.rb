285c114bfe0f2ac57e7566d016331e8352e85508
katoatsu <kato@stamen.co.jp>
2016-03-28 10:05:05 +0000

    create model1

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,23 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    @messages = Message.all
+    #処理
+    #処理
+    #処理
+    #index（テンプレート）を描画
+    render 'index'
   end
+  
+  def create
+    @message = Message.new(message_params)
+    @message.save
+    redirect_to root_path , notice: 'メッセージを保存しました'
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:name, :body)
+  end
+  ##ここまで
 end
